4e1999ec3b7c3d70ce783b8ea12c69071f1a2a83
Vikram Ramavarapu <vikramr2@illinois.edu>
2022-08-26 01:24:52 -0500

    image sizes

--- src/Books.js
+++ src/Books.js
@@ -9,10 +9,10 @@ function Books() {
     return (
         <div class="slide row center2">
             <div class="col">
-                <Link to="/devyani"><img class="book-img" src="https://m.media-amazon.com/images/I/51+6e5U1HML._SY346_.jpg"/></Link>
+                <Link to="/devyani"><img class="book-img" src="https://m.media-amazon.com/images/I/51+6e5U1HML._SY346_.jpg" width='40px' height='auto'/></Link>
             </div>
             <div class="col">
-                <Link to="/azad"><img class="book-img" src="https://m.media-amazon.com/images/I/41imKMm7cgL._AC_SY780_.jpg"/></Link>
+                <Link to="/azad"><img class="book-img" src="https://m.media-amazon.com/images/I/41imKMm7cgL._AC_SY780_.jpg" width='40px' height='auto'/></Link>
             </div>
         </div>
     )
